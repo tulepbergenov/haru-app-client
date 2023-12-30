8e0c2e18f92a9c3eaaedc57bd08244327f47afda
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-12-30 16:25:43 +0600

    feat: add vuex

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from "vue";
 import VApp from "./VApp.vue";
 import { router } from "./router";
+import { store } from "./store";
 
-createApp(VApp).use(router).mount("#app");
+createApp(VApp).use(store).use(router).mount("#app");
